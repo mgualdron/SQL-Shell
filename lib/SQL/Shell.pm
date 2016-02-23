4c3c9948db9f490efe7cc7b4f1f1b214bcc8903b
Miguel Gualdron <dev@gualdron.com>
2016-02-23 09:23:31 -0600

    Updated for 1.15 bug-fix release.

--- lib/SQL/Shell.pm
+++ lib/SQL/Shell.pm
@@ -16,7 +16,7 @@ use IO::File;
 use URI::Escape;
 
 use vars qw($VERSION);
-$VERSION = ('$Revision: 1.14_02 $' =~ /([\d\._]+)/)[0];
+$VERSION = ('$Revision: 1.15 $' =~ /([\d\._]+)/)[0];
 
 use constant HISTORY_SIZE => $ENV{HISTSIZE} || $ENV{HISTFILESIZE} || 50;
 use vars qw(%Renderers %Commands %Settings);
